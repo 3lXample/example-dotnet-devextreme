ed095a64eebf40678cbd48b2a741570007daf410
3lly3fendi <3lly3fendi@gmail.com>
2018-08-20 13:20:42 +0700

    Create Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnet_devextreme.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
