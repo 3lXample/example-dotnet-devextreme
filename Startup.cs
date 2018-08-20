ccda654ccbd81299b7c4e2c7f2b111dccdb7170d
3lly3fendi <3lly3fendi@gmail.com>
2018-08-20 13:21:27 +0700

    Setup Project

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace dotnet_devextreme
 {
@@ -32,7 +33,8 @@ namespace dotnet_devextreme
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
