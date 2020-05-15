6cbb1fa7cd3e5dda38f7f717b7b3a477a6feefd5
Anisetus Elly Efendi <3lly3fendi@gmail.com>
2020-05-15 21:26:42 +0700

    Add DataGrid Example

---
+++ Controllers/DataGridController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_devextreme.Controllers
+{
+    public class DataGridController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/data/customers.js
@@ -0,0 +1,71 @@
+var customers = [{
+  "ID": 1,
+  "FirstName": "Dorian",
+  "LastName": "Buche",
+  "Gender": "Female",
+  "CompanyName": "Minyx",
+  "JobTitle": "Administrative Assistant III"
+}, {
+  "ID": 2,
+  "FirstName": "Herrick",
+  "LastName": "Yellowlees",
+  "Gender": "Male",
+  "CompanyName": "Voomm",
+  "JobTitle": "Account Representative I"
+}, {
+  "ID": 3,
+  "FirstName": "Roi",
+  "LastName": "Macci",
+  "Gender": "Male",
+  "CompanyName": "Quinu",
+  "JobTitle": "Legal Assistant"
+}, {
+  "ID": 4,
+  "FirstName": "Alix",
+  "LastName": "Lodovichi",
+  "Gender": "Female",
+  "CompanyName": "Centidel",
+  "JobTitle": "Health Coach II"
+}, {
+  "ID": 5,
+  "FirstName": "Thayne",
+  "LastName": "Shailer",
+  "Gender": "Male",
+  "CompanyName": "Dabjam",
+  "JobTitle": "Account Representative III"
+}, {
+  "ID": 6,
+  "FirstName": "Ansell",
+  "LastName": "Stryde",
+  "Gender": "Male",
+  "CompanyName": "Digitube",
+  "JobTitle": "Budget/Accounting Analyst I"
+}, {
+  "ID": 7,
+  "FirstName": "Caldwell",
+  "LastName": "Parlett",
+  "Gender": "Male",
+  "CompanyName": "Trudeo",
+  "JobTitle": "Professor"
+}, {
+  "ID": 8,
+  "FirstName": "Tadeo",
+  "LastName": "Livingston",
+  "Gender": "Male",
+  "CompanyName": "InnoZ",
+  "JobTitle": "Physical Therapy Assistant"
+}, {
+  "ID": 9,
+  "FirstName": "Bobbette",
+  "LastName": "Lindermann",
+  "Gender": "Female",
+  "CompanyName": "Devpulse",
+  "JobTitle": "Marketing Assistant"
+}, {
+  "ID": 10,
+  "FirstName": "Lissy",
+  "LastName": "Oldacre",
+  "Gender": "Female",
+  "CompanyName": "Kwimbee",
+  "JobTitle": "Research Associate"
+}];
